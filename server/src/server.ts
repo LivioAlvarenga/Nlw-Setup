2d3c789a674e7a1ba634d68976ce0f19c4471fd6
Livio Alvarenga <livioalvarenga@gmail.com>
2023-01-17 12:09:12 -0300

    criando primeira rota get/hello retornando todos os dados da tabela habit

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,19 @@
-console.log("oi livio");
+import { PrismaClient } from "@prisma/client";
+import Fastify from "fastify";
+
+const app = Fastify();
+const prisma = new PrismaClient();
+
+app.get("/hello", async () => {
+  const habits = await prisma.habit.findMany();
+
+  return habits;
+});
+
+app
+  .listen({
+    port: 3000,
+  })
+  .then(() => {
+    console.log("Listening on port 3000 - HTTP Server running!");
+  });
