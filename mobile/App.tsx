81ffdff34455a6f21f16b69e184b23115fdd7d25
Livio Alvarenga <livioalvarenga@gmail.com>
2023-01-17 15:34:08 -0300

    personalizando a statusbar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,8 +5,7 @@ import {
   Inter_800ExtraBold,
   useFonts,
 } from "@expo-google-fonts/inter";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
 import Loading from "./src/components/Loading";
 
 export default function App() {
@@ -24,7 +23,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
     </View>
   );
 }
