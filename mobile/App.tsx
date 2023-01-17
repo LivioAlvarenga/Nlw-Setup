d2ea0c6be47218258a191b9c7baf27b7b36d313b
Livio Alvarenga <livioalvarenga@gmail.com>
2023-01-17 15:26:58 -0300

    add font inter no app e criando um componente loading, caso a fonte não carregue

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,26 @@
+import {
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  useFonts,
+} from "@expo-google-fonts/inter";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Loading from "./src/components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
@@ -13,11 +32,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#09090a",
     alignItems: "center",
     justifyContent: "center",
   },
   text: {
-    color: "red",
+    color: "#ffffff",
+    fontFamily: "Inter_800ExtraBold",
   },
 });
---
+++ mobile/src/components/Loading.tsx
@@ -0,0 +1,16 @@
+import { ActivityIndicator, View } from "react-native";
+
+export default function Loading() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#09090a",
+      }}
+    >
+      <ActivityIndicator color="#23A3BC" />
+    </View>
+  );
+}
