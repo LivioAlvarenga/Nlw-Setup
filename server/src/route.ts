2e367059f084017f3ccc58c3f52aced7bbc89949
Livio Alvarenga <livioalvarenga@gmail.com>
2023-01-18 11:04:43 -0300

    criado rota habits

--- server/src/route.ts
+++ server/src/route.ts
@@ -1,10 +1,34 @@
+import dayjs from "dayjs";
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
 import { prisma } from "./lib/prisma";
 
 export async function appRoutes(app: FastifyInstance) {
-  app.get("/hello", async () => {
-    const habits = await prisma.habit.findMany();
+  app.post("/habits", async (request) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
 
-    return habits;
+    const { title, weekDays } = createHabitBody.parse(request.body);
+
+    // Zerando as horas, para dar condição de criar o manipular hábitos dia de criação
+    // Se criarmos o hábito as 12:00 e quiser fazer um lançamento do mesmo as 08:00 dará erro
+    // ao zerarmos a hora, damos condição de lançamentos no mesmo dia de criação do hábito
+    const today = dayjs().startOf("day").toDate();
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: today,
+        weekDays: {
+          create: weekDays.map((weekDay) => {
+            return {
+              week_day: weekDay,
+            };
+          }),
+        },
+      },
+    });
   });
 }
