49a67c797134db7e7de0f5af99cd6715ddd21921
Livio Alvarenga <livioalvarenga@gmail.com>
2023-01-18 16:02:07 -0300

    add HabitDay componentes

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,10 +1,48 @@
-import { View } from "react-native";
+import { faker } from "@faker-js/faker";
+import { ScrollView, Text, View } from "react-native";
+import { DAY_SIZE, HabitDay } from "../components/HabitDay";
 import { Header } from "../components/Header";
+import generateDatesFromYearBeginning from "../utils/generate-dates-from-year-beginning";
+
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+const datesFromYearStart = generateDatesFromYearBeginning();
+const minimumSummaryDateSize = 18 * 7; // 18 weeks
+const amountOfDaysToFill = minimumSummaryDateSize - datesFromYearStart.length;
 
 export function Home() {
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
+
+      <View className="mt-6 mb-2 flex-row">
+        {weekDays.map((weekDay) => (
+          <Text
+            key={faker.datatype.uuid()}
+            className="mx-1 text-center text-xl font-bold text-zinc-400"
+            style={{ width: DAY_SIZE }}
+          >
+            {weekDay}
+          </Text>
+        ))}
+      </View>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="flex-row flex-wrap">
+          {datesFromYearStart.map(() => (
+            <HabitDay key={faker.datatype.uuid()} />
+          ))}
+
+          {amountOfDaysToFill > 0 &&
+            Array.from({ length: amountOfDaysToFill }).map(() => (
+              <View
+                className="m-1 rounded-lg border-2 border-zinc-800 bg-zinc-900 opacity-40"
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              />
+            ))}
+        </View>
+      </ScrollView>
     </View>
   );
 }
