4c02a70bd8e623a3c059a7a2fef540bd0293dfd7
Livio Alvarenga <livioalvarenga@gmail.com>
2023-01-18 13:46:43 -0300

    add componentes HabitDay e SummaryTable

---
+++ web/src/components/HabitDay.tsx
@@ -0,0 +1,5 @@
+export default function HabitDay() {
+  return (
+    <div className="h-10 w-10 cursor-pointer rounded-lg border-2 border-zinc-800 bg-zinc-900"></div>
+  );
+}
--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,5 +1,13 @@
 import { faker } from "@faker-js/faker";
+import generateDatesFromYearBeginning from "../utils/generate-dates-from-year-beginning";
+import HabitDay from "./HabitDay";
+
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+
+const summaryDates = generateDatesFromYearBeginning();
+
+const minimumSummaryDateSize = 18 * 7; // 18 weeks
+const amountOfDaysToFill = minimumSummaryDateSize - summaryDates.length;
 
 export default function SummaryTable() {
   return (
@@ -15,6 +23,22 @@ export default function SummaryTable() {
             </div>
           );
         })}
+      </div>
+
+      <div className="grid grid-flow-col grid-rows-7 gap-3">
+        {summaryDates.map((date) => {
+          return <HabitDay key={faker.datatype.uuid()} />;
+        })}
+
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map(() => {
+            return (
+              <div
+                className="h-10 w-10 rounded-lg border-2 border-zinc-800 bg-zinc-900 opacity-40"
+                key={faker.datatype.uuid()}
+              />
+            );
+          })}
       </div>
     </div>
   );
