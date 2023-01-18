f72055785d742d15910242a5372986e68c21d630
Livio Alvarenga <livioalvarenga@gmail.com>
2023-01-18 15:08:25 -0300

    criado o header

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,8 +5,9 @@ import {
   Inter_800ExtraBold,
   useFonts,
 } from "@expo-google-fonts/inter";
-import { StatusBar, StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "react-native";
 import Loading from "./src/components/Loading";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,22 +22,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+    <>
+      <Home />
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#09090a",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "#ffffff",
-    fontFamily: "Inter_800ExtraBold",
-  },
-});
---
+++ mobile/src/components/Header.tsx
@@ -0,0 +1,19 @@
+import { Feather } from "@expo/vector-icons";
+import { Text, TouchableOpacity, View } from "react-native";
+import colors from "tailwindcss/colors";
+import Logo from "../assets/logo.svg";
+
+export function Header() {
+  return (
+    <View className="w-full flex-row items-center justify-between">
+      <Logo />
+      <TouchableOpacity
+        activeOpacity={0.7}
+        className="h-11 flex-row items-center rounded-lg border border-blue-500 px-4"
+      >
+        <Feather name="plus" color={colors.blue[500]} size={20} />
+        <Text className="ml-3 text-base font-semibold text-white">Novo</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
