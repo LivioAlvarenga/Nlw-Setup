5a42636fe39dff889445d332f920903746303336
Livio Alvarenga <livioalvarenga@gmail.com>
2023-01-18 10:08:51 -0300

    separado conexão com DB em arquivo prisma.ts em lib

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,8 @@
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
 import Fastify from "fastify";
+import { prisma } from "./lib/prisma";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors);
 
