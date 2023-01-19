5d62700819ff5e139beb876ab59a30b0d3674e75
Livio Alvarenga <livioalvarenga@gmail.com>
2023-01-19 16:29:20 -0300

    criado checkbox componente

---
+++ mobile/src/components/Checkbox.tsx
@@ -0,0 +1,28 @@
+import { Feather } from "@expo/vector-icons";
+import { Text, TouchableOpacity, TouchableOpacityProps, View } from "react-native";
+import colors from "tailwindcss/colors";
+
+interface Props extends TouchableOpacityProps {
+  checked?: boolean;
+  title: string;
+}
+
+export function Checkbox({ title, checked = false, ...rest }: Props) {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.7}
+      {...rest}
+      className="mb-2 flex-row items-center"
+    >
+      {checked ? (
+        <View className="h-8 w-8 items-center justify-center rounded-lg bg-green-500">
+          <Feather name="check" size={20} color={colors.white} />
+        </View>
+      ) : (
+        <View className="h-8 w-8 rounded-lg bg-zinc-900" />
+      )}
+
+      <Text className="ml-3 text-base text-white">{title}</Text>
+    </TouchableOpacity>
+  );
+}
