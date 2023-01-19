85f692e6b63817c92c7e6b93a4e316fda924934f
Livio Alvarenga <livioalvarenga@gmail.com>
2023-01-19 16:46:34 -0300

    finalizado componente new

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,14 +1,18 @@
 import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 import { Text, TouchableOpacity, View } from "react-native";
 import colors from "tailwindcss/colors";
 import Logo from "../assets/logo.svg";
 
 export function Header() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="w-full flex-row items-center justify-between">
       <Logo />
       <TouchableOpacity
         activeOpacity={0.7}
+        onPress={() => navigate("new")}
         className="h-11 flex-row items-center rounded-lg border border-blue-500 px-4"
       >
         <Feather name="plus" color={colors.blue[500]} size={20} />
--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,9 +1,74 @@
-import { Text, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { faker } from "@faker-js/faker";
+import { useState } from "react";
+import { ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import colors from "tailwindcss/colors";
+import { BackButton } from "../components/BackButton";
+import { Checkbox } from "../components/Checkbox";
+
+const availableWeekDays = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-Feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+];
 
 export function New() {
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  function handleToggleWeekDay(weekDayIndex: number) {
+    if (weekDays.includes(weekDayIndex)) {
+      setWeekDays((prevState) => prevState.filter((weekDay) => weekDay !== weekDayIndex));
+    } else {
+      setWeekDays((prevState) => [...prevState, weekDayIndex]);
+    }
+  }
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
-      <Text>New</Text>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <BackButton />
+
+        <Text className="mt-6 text-3xl font-extrabold text-white">Criar hábito</Text>
+
+        <Text className="mt-6 text-base font-semibold text-white">
+          Qual seu comprometimento?
+        </Text>
+
+        <TextInput
+          placeholder="Exercícios, dormir bem, etc..."
+          placeholderTextColor={colors.zinc[400]}
+          className="mt-3 h-12 rounded-lg bg-zinc-800 pl-4 text-white focus:border-2 focus:border-green-600 "
+        />
+
+        <Text className="mt-4 mb-3 text-base font-semibold text-white">
+          Qual a recorrência?
+        </Text>
+
+        {availableWeekDays.map((weekDay, index) => (
+          <Checkbox
+            key={faker.datatype.uuid()}
+            title={weekDay}
+            checked={weekDays.includes(index)}
+            onPress={() => handleToggleWeekDay(index)}
+          />
+        ))}
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="mt-6 h-14 w-full flex-row items-center justify-center rounded-lg bg-green-600"
+        >
+          <Feather name="check" size={20} color={colors.white} />
+
+          <Text className="ml-2 text-base font-semibold text-white">Confirmar</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </View>
   );
 }
