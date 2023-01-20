270dfc67e8935a200136d24ed88e880e0eb1f620
Livio Alvarenga <livioalvarenga@gmail.com>
2023-01-20 10:28:05 -0300

    add checkbox de dias da semana

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,5 +1,16 @@
+import { faker } from "@faker-js/faker";
+import * as Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
 
+const availableWeekDays = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-Feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+];
 
 export default function NewHabitForm() {
   return (
@@ -16,11 +27,33 @@ export default function NewHabitForm() {
         autoFocus
       />
 
-      <label htmlFor="" className="mt-4 font-semibold leading-tight">
+      <label htmlFor="" className="mt-6 font-semibold leading-tight">
         Qual a recorrência?
       </label>
 
-      <button type="submit" className="mt-6 rounded-lg p-4 items-center justify-center flex gap-3 font-semibold bg-green-600 duration-300 ease-in-out hover:bg-green-500">
+      <div className="mt-3 flex flex-col gap-2">
+        {availableWeekDays.map((weekDay) => {
+          return (
+            <Checkbox.Root
+              key={faker.datatype.uuid()}
+              className="group flex items-center gap-3"
+            >
+              <div className="flex h-8 w-8 items-center justify-center rounded-lg border-2 border-zinc-800 bg-zinc-900 group-data-[state=checked]:border-green-500 group-data-[state=checked]:bg-green-500">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
+
+              <span className=" leading-tight text-white ">{weekDay}</span>
+            </Checkbox.Root>
+          );
+        })}
+      </div>
+
+      <button
+        type="submit"
+        className="mt-6 flex items-center justify-center gap-3 rounded-lg bg-green-600 p-4 font-semibold duration-300 ease-in-out hover:bg-green-500"
+      >
         <Check size={20} weight="bold" />
         Confirmar
       </button>
