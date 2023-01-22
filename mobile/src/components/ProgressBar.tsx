9500eca0c61efa21d9f4e5cf5a2d45f19887d314
Livio Alvarenga <livioalvarenga@gmail.com>
2023-01-22 09:46:00 -0300

    animando a progressBar

--- mobile/src/components/ProgressBar.tsx
+++ mobile/src/components/ProgressBar.tsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 
 interface Props {
   progress?: number;
 }
 
 export default function ProgressBar({ progress = 0 }: Props) {
+  const sharedProgress = useSharedValue(progress);
+
+  const style = useAnimatedStyle(() => {
+    return {
+      width: `${sharedProgress.value}%`,
+    };
+  });
+
+  useEffect(() => {
+    sharedProgress.value = withTiming(progress);
+  }, [progress]);
+
   return (
     <View className="mt-4 h-3 w-full rounded-xl bg-zinc-700">
-      <View className="h-3 rounded-xl bg-blue-600" style={{ width: `${progress}%` }} />
+      <Animated.View className="h-3 rounded-xl bg-blue-600" style={style} />
     </View>
   );
 }
